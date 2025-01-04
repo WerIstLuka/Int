9e8c838cfd9b6bfe613fa115ba761b3d535e9077
Luka <weristluka@gmail.com>
2025-01-04 23:59:34 +0000

    added more options and improved parsing

--- int.go
+++ int.go
@@ -9,6 +9,21 @@ import (
 	"strconv"
 )
 
+var Version string = "2.0-dev"
+
+func Help(){
+	println(`Convert any base to any other
+Usage:
+	int [Options] [Integers]
+Options:
+	-b	add binary prefix to all integers
+	-x	add hexadecimal prefix to all integers
+	-o	add octal prefix to all integers
+	-Bx	where x is the base of the integers
+	-Ox	set the output to any base, also works with b, x and o
+	-h	show this Help text`)
+}
+
 func HasPipeInput()bool{
 	FileInfo, _ := os.Stdin.Stat()
 	return FileInfo.Mode() & os.ModeCharDevice == 0
@@ -65,21 +80,32 @@ func Parser(Options []string)(uint64, uint64){
 		Option := Options[i]
 		if len(Option) < 2{
 			println("Error: invalid option \"", Option, "\"")
+			os.Exit(1)
 		}
 		if InputBase != 0 && slices.Contains([]string{"-B", "-b", "-o", "-x"}, Option){
 			println("Error: Input base was given twice")
 			os.Exit(1)
-		}
-		if Option[1:2] == "B" && digitCheck.MatchString(Option[2:len(Option)-1]){
-			InputBase, _ = strconv.ParseUint(Option[2:len(Option)-1], 10, 64)
+		} else if len(Option) == 2 && slices.Contains([]string{"b", "o", "x"}, Option[1:2]){
+			InputBase = GetInt(Option[1:2])
+		} else if GotInputBase && InputBase == 0{
+			println("Error: input base can't be 0")
+			os.Exit(1)
+		} else if Option == "-h" || Option == "--help"{
+			Help()
+		} else if Option == "-v" || Option == "--version"{
+			println(Version)
+			os.Exit(0)
+		} else if len(Option) < 3{
+			println("Error: too few arguments: ", Option)
+			os.Exit(1)
+		} else if Option[1:2] == "B" && digitCheck.MatchString(Option[2:len(Option)]){
+			println("got a match: ", Option)
+			InputBase, _ = strconv.ParseUint(Option[2:len(Option)], 10, 64)
 			GotInputBase = true
 		}
-		if len(Option) == 2 && slices.Contains([]string{"b", "o", "x"}, Option[1:2]){
-			InputBase = GetInt(Option[1:2])
-		}
-		if GotInputBase && InputBase == 0{
-			println("Error: input base can't be 0")
-		}
+		println(Option[1:2])
+		println(Option[2:len(Option)])
+		println(digitCheck.MatchString(Option[2:len(Option)]))
 	println(InputBase)
 	println(OutputBase)
 	}
