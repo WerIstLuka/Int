9e2a50fe4d42af479f99193370806810ea988c91
Luka <weristluka@gmail.com>
2025-01-18 14:38:12 +0000

    improved parsing

--- int.go
+++ int.go
@@ -22,6 +22,7 @@ Options:
 	-Bx	where x is the base of the integers
 	-Ox	set the output to any base, also works with b, x and o
 	-h	show this Help text`)
+	os.Exit(0)
 }
 
 func HasPipeInput()bool{
@@ -67,53 +68,103 @@ func GetInt(char string)uint64{
 		default:
 			return 0
 	}
-	return 0
+}
+
+func GetBase(Option string)uint64{
+	var Base uint64
+	digitCheck := regexp.MustCompile(`^[0-9]+$`)
+	if digitCheck.MatchString(Option){
+		Base, _ = strconv.ParseUint(Option, 10, 64)
+	} else {
+		Base = GetInt(Option)
+	}
+	if Base == 0{
+		println("Error: invalid base:", Option)
+		os.Exit(1)
+	}
+	return Base
 }
 
 
 func Parser(Options []string)(uint64, uint64){
-	digitCheck := regexp.MustCompile(`^[0-9]+$`)
 	var GotInputBase bool = false
+	var GotOutputBase bool = false
 	var InputBase uint64 = 0
 	var OutputBase uint64 = 0
 	for i:=0; i<len(Options); i++{
 		Option := Options[i]
+		println(Option)
 		if len(Option) < 2{
-			println("Error: invalid option \"", Option, "\"")
+			println("Error: invalid option:", Option)
 			os.Exit(1)
 		}
 		if InputBase != 0 && slices.Contains([]string{"-B", "-b", "-o", "-x"}, Option){
 			println("Error: Input base was given twice")
 			os.Exit(1)
+		} else if OutputBase != 0 && Option[2:len(Option)] == "O"{
+			println("Error: Output base was given twice")
+			os.Exit(1)
 		} else if len(Option) == 2 && slices.Contains([]string{"b", "o", "x"}, Option[1:2]){
 			InputBase = GetInt(Option[1:2])
 		} else if GotInputBase && InputBase == 0{
-			println("Error: input base can't be 0")
+			println("Error: Input base can't be 0")
+			os.Exit(1)
+		} else if GotOutputBase && OutputBase ==0{
+			println("Error: Output base can't be 0")
 			os.Exit(1)
 		} else if Option == "-h" || Option == "--help"{
 			Help()
 		} else if Option == "-v" || Option == "--version"{
 			println(Version)
 			os.Exit(0)
-		} else if len(Option) < 3{
-			println("Error: too few arguments: ", Option)
+		} else if slices.Contains([]string{"B", "O"}, Option[1:2]) == false{
+			println("Error: invalid option:", Option)
 			os.Exit(1)
-		} else if Option[1:2] == "B" && digitCheck.MatchString(Option[2:len(Option)]){
-			println("got a match: ", Option)
-			InputBase, _ = strconv.ParseUint(Option[2:len(Option)], 10, 64)
+		} else if len(Option) < 3{
+			println("Error: too few arguments:", Option)
+			println(Option[1:2])
+			os.Exit(1)
+		} else if Option[1:2] == "B"{
+			InputBase = GetBase(Option[2:len(Option)])
 			GotInputBase = true
+		} else if Option[1:2] == "O"{
+			OutputBase = GetBase(Option[2:len(Option)])
+			GotOutputBase = true
 		}
-		println(Option[1:2])
-		println(Option[2:len(Option)])
-		println(digitCheck.MatchString(Option[2:len(Option)]))
-	println(InputBase)
-	println(OutputBase)
 	}
+	println("In:", InputBase)
+	println("Out:", OutputBase)
 	return InputBase, OutputBase
 }
 
+func ConvertNumbers(Numbers []string, InputBase uint64) {
+	ConvertedNumbers := []uint64{}
+	for i:=0; i<len(Numbers); i++{
+		NumberStr := Numbers[i]
+		println(NumberStr)
+		if len(NumberStr) >= 2{
+			switch NumberStr[0:2]{
+				case "0b":
+					InputBase = 2
+			}
+		}
+		NumberInt, err := strconv.ParseUint(NumberStr, 10, 64)
+		println(err)
+		if err == nil{
+			println("Operation not possible:", NumberStr)
+			os.Exit(1)
+		}
+		println(NumberInt)
+		ConvertedNumbers = append(ConvertedNumbers, NumberInt)
+	}
+}
 
 func main() {
-	Options, _ := GetArguments()
-	Parser(Options)
+	Options, Numbers := GetArguments()
+	InputBase, _ := Parser(Options)
+	if len(Numbers) == 0{
+		println("no input")
+		os.Exit(0)
+	}
+	ConvertNumbers(Numbers, InputBase)
 }
